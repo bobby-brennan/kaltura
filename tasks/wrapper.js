817c0d11c49f89efe3bb86aaa28004eeee7aec4b
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2014-12-26 19:06:55 +0000

    Add kaltura wrapper

--- tasks/wrapper.js
+++ tasks/wrapper.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var FS = require('fs');
+var EJS = require('ejs');
 
 module.exports = function (grunt) {
   grunt.registerTask('kaltura_wrapper', 'A grunt plugin for generating wrappers the NYTimes API', function () {
@@ -10,12 +11,19 @@ module.exports = function (grunt) {
     console.log('dir:' + __dirname + '/../kaltura');
     var files = FS.readdirSync(__dirname + '/../kaltura');
     console.log('files;' + files.length);
-    FS.mkdirSync('kaltura')
+    FS.mkdirSync(config.lucyDir);
+    FS.mkdirSync(config.lucyDir + '/kaltura');
     for (var i = 0; i < files.length; ++i) {
-      FS.createReadStream(__dirname + '/../kaltura/' + files[i]).pipe(FS.createWriteStream('kaltura/' + files[i]));
+      FS.createReadStream(__dirname + '/../kaltura/' + files[i]).pipe(FS.createWriteStream(config.lucyDir + '/kaltura/' + files[i]));
     }
-    grunt.file.expand(__dirname + '/../node_modules/lucy-rest-api-client/tasks').forEach(function(inpt) {console.log('tsk:' + inpt); grunt.loadTasks(inpt)});
-    grunt.config('rest_api_client', {default_config: config});
-    grunt.task.run('rest_api_client');
+    config.clientFile = 'kaltura-raw-api.js';
+
+    EJS.renderFile(__dirname + '/templates/node/node.ejs', {}, function(err, rendered) {
+      FS.writeFileSync(config.lucyDir + '/' + config.clientFile, rendered);
+
+      grunt.file.expand(__dirname + '/../node_modules/lucy-rest-api-client/tasks').forEach(function(inpt) {console.log('tsk:' + inpt); grunt.loadTasks(inpt)});
+      grunt.config('rest_api_client', {default_config: config});
+      grunt.task.run('rest_api_client');
+    });
   })
 }
